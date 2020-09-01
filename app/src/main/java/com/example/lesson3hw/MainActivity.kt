c5fcd50d6b610986ff9b180fb6e13564c668bae6
mHeakal <heakalmohammed@gmail.com>
2020-09-01 17:05:57 -0500

    First commit

---
+++ app/src/main/java/com/example/lesson3hw/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.lesson3hw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var foods: MutableList<String> = listOf(
+        "Hamburger", "Pizza",
+        "Mexican", "American", "Chinese"
+    ).toMutableList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun selectRandomFood(view: View) {
+        var random = Math.random() * foods.size
+        textView.text = foods.get(random.toInt())
+    }
+
+    fun addFood(view: View) {
+        if (!addFoodText.text.toString().equals(""))
+            foods.add(addFoodText.text.toString())
+    }
+}
